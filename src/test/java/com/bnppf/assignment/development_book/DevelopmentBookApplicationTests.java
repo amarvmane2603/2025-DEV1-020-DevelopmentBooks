aa0194fe407a6c73552d653c4a1d0e9fd673e7b1
amarmane <amarvmane2603@gmail.com>
2026-08-13 23:42:57 +0200

    chore: initialize Spring Boot project skeleton

---
+++ src/test/java/com/bnppf/assignment/development_book/DevelopmentBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnppf.assignment.development_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
